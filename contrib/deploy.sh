36fcf8d6b312f2eead5239dad8a6fc4686ec9e1f
DmitryBurnaev <dmitry.burnaev@gmail.com>
2020-12-20 17:00:07 +0300

    [#19] CD: github actions (deploy actions)

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-echo "${REGISTRY_URL}" > ~/deploy.log
+echo "${REGISTRY_URL}_111" > ~/deploy.log
 supervisorctl stop podcast_service:
 docker-compose down
 supervisorctl start podcast_service:
 echo y | docker image prune -a
 supervisorctl status podcast_service:
+exit 0
