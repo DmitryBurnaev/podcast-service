be434da5c7c3aed85455382d3b973eff3a5861f3
DmitryBurnaev <dmitry.burnaev@gmail.com>
2020-12-21 10:17:14 +0300

    [#19] CD: github actions (deploy actions)

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-echo "${REGISTRY_URL}_111" > ./deploy.log
-echo "${PROD_SSH_HOST}_222" >> ./deploy.log
-
+echo "Pulling image ${REGISTRY_URL}..." > ./deploy.log
 docker pull ${REGISTRY_URL}/podcast-service:last
+
+echo "Restarting service..." > ./deploy.log
 supervisorctl stop podcast_service:
 docker-compose down
 supervisorctl start podcast_service:
+
+echo "Clearing..." > ./deploy.log
 echo y | docker image prune -a
 supervisorctl status podcast_service:
 exit 0
