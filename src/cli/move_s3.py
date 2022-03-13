c5cc4bab8011f5743b5f1cca6a5e63352918158d
DmitryBurnaev <dmitry.burnaev@gmail.com>
2022-03-13 16:13:08 +0300

    [#98] Preparing for moving script

--- src/cli/move_s3.py
+++ src/cli/move_s3.py
@@ -1,12 +1,22 @@
-from common.storage import StorageS3
+import asyncio
+
+import aioboto3
+
 from core import settings
 
 
-def main():
+async def main():
     print(f" ===== Running moving ===== ")
-    s1 = StorageS3()
-
+    session = aioboto3.Session(
+        aws_access_key_id=settings.S3_AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.S3_AWS_SECRET_ACCESS_KEY,
+        region_name="ru-central1",
+    )
+    async with session.resource("s3") as s3:
+        bucket = await s3.Bucket(settings.S3_BUCKET_NAME)
+        async for s3_object in bucket.objects.all():
+            print(s3_object)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
