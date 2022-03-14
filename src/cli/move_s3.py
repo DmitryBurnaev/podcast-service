76fc3e3fe642c3ee62bfc89ce5251ece64f753ae
DmitryBurnaev <dmitry.burnaev@gmail.com>
2022-03-14 09:51:48 +0300

    [#98] Preparing for moving script (step3)

--- src/cli/move_s3.py
+++ src/cli/move_s3.py
@@ -1,8 +1,12 @@
 import asyncio
+import logging
 
 import aioboto3
 
 from core import settings
+
+# ...S3_CONFIG_FROM
+# ...S3_CONFIG_TO
 
 
 async def main():
@@ -12,11 +16,22 @@ async def main():
         aws_secret_access_key=settings.S3_AWS_SECRET_ACCESS_KEY,
         region_name="ru-central1",
     )
-    async with session.resource("s3") as s3:
+    print("session", session)
+
+    objects = []
+    async with session.resource("s3", endpoint_url=settings.S3_STORAGE_URL) as s3:
         bucket = await s3.Bucket(settings.S3_BUCKET_NAME)
         async for s3_object in bucket.objects.all():
             print(s3_object)
+            if not s3_object.key.endswith('/'):
+                objects.append(s3_object)
+                break
 
+    async with session.client("s3", endpoint_url=settings.S3_STORAGE_URL) as s3:
+        for obj in objects:
+            with open(settings.PROJECT_ROOT_DIR / 'media/s3' / obj.key, 'wb') as file:
+                await s3.download_fileobj(settings.S3_BUCKET_NAME, obj.key, file)
+                # await s3.upload_fileobj(spfp, bucket, blob_s3_key)
 
 if __name__ == "__main__":
     asyncio.run(main())
