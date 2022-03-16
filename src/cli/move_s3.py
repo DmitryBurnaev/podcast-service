cdaa9456e7359ca2b71d4f170a5b72295197e2b6
DmitryBurnaev <dmitry.burnaev@gmail.com>
2022-03-16 10:14:55 +0300

    [#98] Full file process setup (step1)

--- src/cli/move_s3.py
+++ src/cli/move_s3.py
@@ -9,13 +9,38 @@ from core import settings
 
 # ...S3_CONFIG_FROM
 # ...S3_CONFIG_TO
-
+DOWNLOAD_DIR = settings.PROJECT_ROOT_DIR / 'media/s3'
 
 logger = get_logger(__name__)
 
 
 async def progress_as_completed(tasks):
     return [await task for task in tqdm.asyncio.tqdm.as_completed(tasks)]
+
+
+async def download_object(s3, obj_key: str):
+    await s3.download_file(settings.S3_BUCKET_NAME, obj_key, filename=(DOWNLOAD_DIR / obj_key))
+    # with open(DOWNLOAD_DIR / obj_key, 'wb') as file:
+    #     await s3.download_fileobj(settings.S3_BUCKET_NAME, obj_key, file)
+
+
+async def upload_object(s3, obj_key: str):
+    await s3.upload_file((DOWNLOAD_DIR / obj_key), settings.S3_BUCKET_NAME, obj_key)
+
+
+async def check_object(s3, obj_key: str, expected_size):
+    response = await s3.meta.client.head_object(
+        Bucket=settings.S3_BUCKET_NAME, Key=obj_key)
+
+    assert response.get('ContentLength', 0) == expected_size
+
+# async def delete_object(s3, obj_key: str):
+#
+
+async def process_file(s3, obj_key):
+    await download_object(s3, obj_key)
+    await upload_object(s3, obj_key)
+    await check_object(s3, obj_key, expected_size=10)  # TODO: get expected size from episode
 
 
 async def main():
@@ -39,12 +64,7 @@ async def main():
     tasks = []
     async with session.client("s3", endpoint_url=settings.S3_STORAGE_URL) as s3:
         for obj in objects:
-            with open(settings.PROJECT_ROOT_DIR / 'media/s3' / obj.key, 'wb') as file:
-                tasks.append(
-                    s3.download_fileobj(settings.S3_BUCKET_NAME, obj.key, file)
-                )
-                # await s3.download_fileobj(settings.S3_BUCKET_NAME, obj.key, file)
-                # await s3.upload_fileobj(spfp, bucket, blob_s3_key)
+            tasks.append(process_file(s3, obj.key))
 
     # TODO: Limit with max downloads per time
     await progress_as_completed(tasks)
