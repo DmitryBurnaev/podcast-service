682c6cf28e4d5e8b330f8669fa802aa92155af15
DmitryBurnaev <dmitry.burnaev@gmail.com>
2022-04-13 10:03:16 +0300

    [#101] Logic updates for new S3: small updates for file's fields

--- src/modules/media/models.py
+++ src/modules/media/models.py
@@ -21,11 +21,11 @@ class File(ModelBase, ModelMixin):
     type = EnumTypeColumn(FileType)
     path = Column(String(length=256))
     size = Column(Integer, default=0)
+    source_url = Column(String(length=512))
     available = Column(Boolean, default=False)
     access_token = Column(String(length=128), index=True)
     owner_id = Column(Integer, ForeignKey("auth_users.id"), index=True)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     def __str__(self):
         return f'<File #{self.id} "{self.path}">'
