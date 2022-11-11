9c2fbdaa4fd586068ba6d9eda90ee9fecc1f75e7
DmitryBurnaev <dmitry.burnaev@gmail.com>
2022-11-11 18:50:26 +0400

    [152] Single episode's WS progress: removed old (HTTP-based) API

--- src/modules/podcast/views/progress.py
+++ src/modules/podcast/views/progress.py
@@ -6,69 +6,13 @@ import aioredis
 import async_timeout
 from starlette.websockets import WebSocket
 
-from common.views import BaseHTTPEndpoint, BaseWSEndpoint
+from common.views import BaseWSEndpoint
 from core import settings
 from modules.podcast.models import Podcast, Episode
 from modules.podcast.schemas import ProgressResponseSchema, WSProgressRequestSchema
 from modules.podcast.utils import check_state
 
 logger = logging.getLogger(__name__)
-
-
-class ProgressAPIView(BaseHTTPEndpoint):
-    """
-    Temp solution (web-socket for poor) to quick access to downloading process
-    (statistic is saved in Redis)
-    # TODO: Rewrite this to web-socket
-    """
-
-    schema_response = ProgressResponseSchema
-
-    async def get(self, request):
-        podcast_items = {
-            podcast.id: podcast
-            for podcast in await Podcast.async_filter(self.db_session, owner_id=request.user.id)
-        }
-        episodes = {
-            episode.id: episode
-            for episode in await Episode.get_in_progress(self.db_session, request.user.id)
-        }
-        progress = await check_state(episodes.values())
-
-        for progress_item in progress:
-            podcast: Podcast = podcast_items.get(progress_item.pop("podcast_id"))
-            episode: Episode = episodes.get(progress_item.pop("episode_id"))
-            progress_item["episode"] = {
-                "id": episode.id,
-                "title": episode.title,
-                "image_url": episode.image_url,
-                "status": episode.status,
-            }
-            progress_item["podcast"] = {
-                "id": podcast.id,
-                "name": podcast.name,
-                "image_url": podcast.image_url,
-            }
-
-        return self._response(data=progress)
-
-
-class EpisodeInProgressAPIView(BaseHTTPEndpoint):
-    """Current downloading progress for requested episode"""
-
-    schema_response = ProgressResponseSchema
-    db_model = Episode
-
-    async def get(self, request):
-        episode_id = request.path_params["episode_id"]
-        episode = await self._get_object(episode_id)
-        progress_data = {}
-        if episode.status in Episode.PROGRESS_STATUSES:
-            if progress := await check_state([episode]):
-                progress_data = progress[0]
-
-        progress_data["episode"] = episode
-        return self._response(progress_data)
 
 
 class ProgressWS(BaseWSEndpoint):
