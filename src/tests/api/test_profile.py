ccbe5bab820e5e8df0a20fdb7fba5f9a97dddfdb
DmitryBurnaev <dmitry.burnaev@gmail.com>
2022-12-10 20:28:49 +0400

    [#167] User's patch: tests updated

--- src/tests/api/test_profile.py
+++ src/tests/api/test_profile.py
@@ -1,8 +1,13 @@
+import pytest
+from starlette_web.common.http.statuses import ResponseStatus
+
+from modules.auth.utils import encode_jwt, TOKEN_TYPE_RESET_PASSWORD, TOKEN_TYPE_REFRESH
+from tests.api.test_auth import assert_tokens, INVALID_CHANGE_PASSWORD_DATA
 from tests.api.test_base import BaseTestAPIView
 from tests.helpers import await_
 
 
-class TestAuthMeAPIView(BaseTestAPIView):
+class TestProfileAPIView(BaseTestAPIView):
     url = "/api/auth/me/"
 
     def test_get__ok(self, client, user):
@@ -29,3 +34,74 @@ class TestAuthMeAPIView(BaseTestAPIView):
 
         await_(dbs.refresh(user))
         assert user.email == "new-user@test.com"
+
+
+class TestChangePasswordAPIView(BaseTestAPIView):
+    url = "/api/auth/change-password/"
+    new_password = "new123456"
+
+    def _assert_fail_response(self, client, token: str, response_status: str = None) -> dict:
+        request_data = {
+            "token": token,
+            "password_1": self.new_password,
+            "password_2": self.new_password,
+        }
+        client.logout()
+        response = client.post(self.url, json=request_data)
+        assert response.status_code == 401
+        response_data = response.json()
+        if response_status:
+            assert response_data["status"] == response_status
+        return response.json()["payload"]
+
+    def test_change_password__ok(self, client, user, user_session, dbs):
+        token, _ = encode_jwt(
+            {"user_id": user.id, "session_id": user_session.public_id},
+            token_type=TOKEN_TYPE_RESET_PASSWORD,
+        )
+        request_data = {
+            "token": token,
+            "password_1": self.new_password,
+            "password_2": self.new_password,
+        }
+        client.logout()
+        response = client.post(self.url, json=request_data)
+        response_data = self.assert_ok_response(response)
+        assert_tokens(response_data, user)
+
+        await_(dbs.refresh(user))
+        assert user.verify_password(self.new_password)
+
+    @pytest.mark.parametrize("invalid_data, error_details", INVALID_CHANGE_PASSWORD_DATA)
+    def test_invalid_request__fail(self, client, invalid_data: dict, error_details: dict):
+        client.logout()
+        self.assert_bad_request(client.post(self.url, json=invalid_data), error_details)
+
+    def test__token_expired__fail(self, client, user):
+        token, _ = encode_jwt({"user_id": user.id}, expires_in=-10)
+        response_data = self._assert_fail_response(client, token, ResponseStatus.AUTH_FAILED)
+        self.assert_auth_invalid(response_data, "JWT signature has been expired for token")
+
+    def test__token_invalid_type__fail(self, client, user):
+        token, _ = encode_jwt({"user_id": user.id}, token_type=TOKEN_TYPE_REFRESH)
+        response_data = self._assert_fail_response(client, token)
+        self.assert_auth_invalid(
+            response_data, "Token type 'reset_password' expected, got 'refresh' instead."
+        )
+
+    def test_token_invalid__fail(self, client, user):
+        response_data = self._assert_fail_response(client, "fake-jwt")
+        self.assert_auth_invalid(response_data, "Token could not be decoded: Not enough segments")
+
+    def test_user_inactive__fail(self, client, user, dbs):
+        await_(user.update(dbs, is_active=False))
+        await_(dbs.commit())
+        token, _ = encode_jwt({"user_id": user.id}, token_type=TOKEN_TYPE_RESET_PASSWORD)
+        response_data = self._assert_fail_response(client, token)
+        self.assert_auth_invalid(response_data, f"Couldn't found active user with id={user.id}.")
+
+    def test_user_does_not_exist__fail(self, client, user):
+        user_id = 0
+        token, _ = encode_jwt({"user_id": user_id}, token_type=TOKEN_TYPE_RESET_PASSWORD)
+        response_data = self._assert_fail_response(client, token)
+        self.assert_auth_invalid(response_data, f"Couldn't found active user with id={user_id}.")
