773b022ffa5e79826016e3fe6c854a7187329b13
DmitryBurnaev <dmitry.burnaev@gmail.com>
2023-05-08 15:51:56 +0300

    [#202] Cancel episode downloading: small experiment added

--- src/kill_subpr_experiments.py
+++ src/kill_subpr_experiments.py
@@ -1,40 +1,45 @@
 import multiprocessing
+import random
 import time
 
 
 def func(number):
     print("f")
+    a = 0
     for i in range(1, 10):
-        time.sleep(1)
-        print('Processing ' + str(number) + ': prints ' + str(number * i))
+        time.sleep(0.3)
+        a += random.randint(1, 100)
+        print('Processing ' + str(number) + f': prints | {a=} | ' + str(number * i))
 
-#
-#         def worker(return_dict):
-#             """worker function"""
-#             print(str(procnum) + " represent!")
-#             return_dict[procnum] = procnum
-#
-#         run_task_function = partial(asyncio.run, self._perform_and_run(*args, **kwargs))
-#         sub_process = multiprocessing.Process(target=run_task_function)
-#         sub_process.start()
-#         sub_process.
+    return a
+
+
+terminate_me = False
+
+
+def put_result(queue, number):
+    result = func(number)
+    queue.put(result)
+
+
+def run_and_terminate():
+    result_queue = multiprocessing.Queue()
+
+    process = multiprocessing.Process(target=put_result, args=(result_queue, 10))
+    process.start()
+
+    # run_subprocess(result_queue)
+    print("waiting...")
+    time.sleep(random.randint(2, 3))
+    if process.is_alive():
+        process.terminate()
+        print("terminated")
+        return
+
+    print("result: ", result_queue.get())
 
 
 if __name__ == '__main__':
-    # list of all processes, so that they can be killed afterwards
-    print("start")
-    all_processes = []
-
-    for i in range(0, 3):
-        print(f"start {i}")
-        process = multiprocessing.Process(target=func, args=(i,))
-        process.start()
-        all_processes.append(process)
-
-    # kill all processes after 0.03s
-    time.sleep(2)
-    for process in all_processes:
-        print(f"terminating {process}")
-        process.terminate()
+    run_and_terminate()
 
 
