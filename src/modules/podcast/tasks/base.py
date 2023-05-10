ff981819b788af463c77027fd6c91982fd650558
DmitryBurnaev <dmitry.burnaev@gmail.com>
2023-05-10 18:52:51 +0300

    [#202] Cancel episode downloading: refactored base task's module

--- src/modules/podcast/tasks/base.py
+++ src/modules/podcast/tasks/base.py
@@ -2,6 +2,7 @@ import enum
 import asyncio
 import logging
 import multiprocessing
+import queue
 import time
 
 from redis.client import Redis
@@ -9,7 +10,6 @@ from rq.job import Job
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from common.db_utils import make_session_maker
-from common.exceptions import BaseApplicationError
 
 logger = logging.getLogger(__name__)
 
@@ -18,10 +18,6 @@ class FinishCode(int, enum.Enum):
     OK = 0
     SKIP = 1
     ERROR = 2
-
-
-class TaskCancelledError(BaseApplicationError):
-    message = "Task was cancelled"
 
 
 class RQTask:
@@ -36,40 +32,9 @@ class RQTask:
         """We need to override this method to implement main task logic"""
         raise NotImplementedError
 
-    def _run_and_return_result(self, queue, *args, **kwargs):
-        finish_code = asyncio.run(self._perform_and_run(*args, **kwargs))
-        queue.put(finish_code)
-
-    @staticmethod
-    def _get_finish_code_from_queue(queue):
-        try:
-            return queue.get(block=False)
-        except Exception as exc:
-            return None
-
     def __call__(self, *args, **kwargs) -> FinishCode:
         logger.info("==== STARTED task %s ====", self.name)
-
-        result_queue = multiprocessing.Queue()
-        process = multiprocessing.Process(
-            target=self._run_and_return_result,
-            args=(result_queue, *args),
-            kwargs=kwargs,
-        )
-        process.start()
-
-        job = Job.fetch(self.get_job_id(**kwargs), connection=Redis())
-        print("while")
-
-        while not (finish_code := self._get_finish_code_from_queue(result_queue)):
-            status = job.get_status()
-            print("jobid: ", job.id, "status:", status)
-            if status == "canceled":
-                process.terminate()
-                print(f"Process {process} terminated!")
-                break
-            time.sleep(1)
-
+        finish_code = self._run_with_subprocess(*args, **kwargs)
         logger.info("==== FINISHED task %s | code %s ====", self.name, finish_code)
         return finish_code
 
@@ -80,8 +45,6 @@ class RQTask:
     async def _perform_and_run(self, *args, **kwargs):
         """Allows calling `self.run` in transaction block with catching any exceptions"""
 
-        # TODO: run asyncio task for killing task (if some flag is placed in the job's metadata)
-
         session_maker = make_session_maker()
         try:
             async with session_maker() as db_session:
@@ -89,16 +52,45 @@ class RQTask:
                 result = await self.run(*args, **kwargs)
                 await self.db_session.commit()
 
-        except TaskCancelledError as exc:
-            await self.teardown()
-            logger.warning("Task was canceled by err: %r", exc)
-
         except Exception as exc:
             await self.db_session.rollback()
             result = FinishCode.ERROR
             logger.exception("Couldn't perform task %s | error %r", self.name, exc)
 
         return result
+
+    def _run_and_return_result(self, queue, *args, **kwargs):
+        finish_code = asyncio.run(self._perform_and_run(*args, **kwargs))
+        queue.put(finish_code)
+
+    def _run_with_subprocess(self, *task_args, **task_kwargs) -> FinishCode:
+        """ Run logic in subprocess allows to terminate run task in the background"""
+
+        def _get_finish_code_from_queue(result_queue: multiprocessing.Queue):
+            try:
+                return result_queue.get(block=False)
+            except queue.Empty:
+                return None
+
+        result_queue = multiprocessing.Queue()
+        process = multiprocessing.Process(
+            target=self._run_and_return_result,
+            args=(result_queue, *task_args),
+            kwargs=task_kwargs,
+        )
+        process.start()
+
+        job = Job.fetch(self.get_job_id(**task_kwargs), connection=Redis())
+        while not (finish_code := _get_finish_code_from_queue(result_queue)):
+            status = job.get_status()
+            print("jobid: ", job.id, "status:", status)
+            if status == "canceled":  # status can be changed by RQTask.cancel_task()
+                process.terminate()
+                print(f"Process {process} terminated!")
+                break
+            time.sleep(1)
+
+        return finish_code
 
     @property
     def name(self):
@@ -126,7 +118,3 @@ class RQTask:
             logger.exception("Couldn't cancel task %s: %r", job_id, exc)
         else:
             logger.info("Canceled task %s", job_id)
-
-    async def teardown(self):
-        # TODO: perform some actions for graceful stopping task and run subprocess (by pid maybe?)
-        ...
