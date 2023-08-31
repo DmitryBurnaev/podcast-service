2799318a51ba063ddc63f8437ab64534a0bce8ff
DmitryBurnaev <dmitry.burnaev@gmail.com>
2023-08-31 19:06:27 +0300

    [#202] Cancel episode downloading: added simple examples for killing subprocess

--- src/kill_subpr_experiments.py
+++ src/kill_subpr_experiments.py
@@ -54,6 +54,59 @@ def run_and_terminate():
     print("result: ", result_queue.get())
 
 
+def kill_with_sigint():
+    time.sleep(10)
+    print("killing")
+    os.system(f"ps aux | grep \"ffmpeg\" | grep -v grep | awk '{{print $2}}' | xargs kill -SIGINT")
+    print("done killing")
+
+
+def run_with_sigkill():
+
+    # result_queue = multiprocessing.Queue()
+    # process = multiprocessing.Process(target=kill_with_sigint)
+    # process.start()
+    # process = multiprocessing.Process(target=put_result, args=(result_queue, 10))
+
+    # run_subprocess(result_queue)
+    # print("waiting...")
+    # time.sleep(random.randint(1, 2))
+    # if process.is_alive():
+    #     grep = "sleep 125"
+    #     print(f"ps aux | grep \"{grep}\" | grep -v grep | awk '{{print $2}}' | xargs kill ")
+    #     os.system(f"ps aux | grep \"{grep}\" | grep -v grep | awk '{{print $2}}' | xargs kill")
+    #     process.terminate()
+    #     print("terminated")
+    #     return
+    #
+    # print("result: ", result_queue.get())
+    completed_proc = None
+    try:
+        ffmpeg_params = ["-vn", "-acodec", "libmp3lame", "-q:a", "5"]
+        completed_proc = subprocess.run(
+            ["ffmpeg", "-y", "-i", "../.misc/audio/prolog.mp3", *ffmpeg_params, "../.misc/audio/tmp_prolog.mp3"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            check=True,
+            timeout=120,
+        )
+    except subprocess.CalledProcessError as err:
+        err_details = f"FFMPEG cancelled??: {err.returncode}"
+        raise RuntimeError(err_details) from err
+
+    except Exception as exc:
+        err_details = f"FFMPEG failed with exit status: {completed_proc.returncode}"
+        raise RuntimeError(err_details) from exc
+
+    else:
+        # process.terminate()
+        print(
+            "FFMPEG success done preparation for file %s:\n%s",
+            completed_proc.returncode,
+            str(completed_proc.stdout, encoding="utf-8"),
+        )
+
+
 def run_too_long_process(sleep_time: int):
     print("Start sleeping process")
     time.sleep(sleep_time)
@@ -62,5 +115,5 @@ def run_too_long_process(sleep_time: int):
 
 if __name__ == "__main__":
     # run_bash_script()
-    run_and_terminate()
+    run_with_sigkill()
     # run_too_long_process(120)
