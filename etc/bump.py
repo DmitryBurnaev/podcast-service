3e34b8ff0c9b5deee394307b8ce9fa770bb8199f
DmitryBurnaev <dmitry.burnaev@gmail.com>
2025-01-25 18:58:25 +0300

    [#280] Improve bump dependency script: use git logic

--- etc/bump.py
+++ etc/bump.py
@@ -41,17 +41,29 @@ def create_tmp_git_branch(branch_name: str) -> None:
     # git_dir
 
 
+def git_commit_and_push(branch_name: str) -> None:
+    repo = git.Repo.init(repo_dir)
+    past_branch = repo.create_head(branch_name, default_branch)
+    past_branch.checkout()
+    # TODO: make commit here
+    # past_branch.commit()
+
+
+def git_push(branch_name: str) -> None:
+    repo = git.Repo.init(repo_dir)
+    past_branch = repo.create_head(branch_name, default_branch)
+    past_branch.push()
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--lib_name", required=True, type=str)
-    parser.add_argument("--old_version", required=True, type=str)
-    parser.add_argument("--new_version", required=True, type=str)
+    parser.add_argument("libs", required=True, type=str, action="append")
     args = parser.parse_args()
-    lib_name = args.lib_name
-    old_version = args.old_version
-    new_version = args.new_version
-    replace_version_in_file(lib_name, old_version, new_version)
-    create_tmp_git_branch(args.lib_name)
+    libs = args.libs
+    print(libs)
+    for lib in libs:
+        replace_version_in_file(lib, find_version_in_file(lib), default_branch)
+
     return None
 
 
